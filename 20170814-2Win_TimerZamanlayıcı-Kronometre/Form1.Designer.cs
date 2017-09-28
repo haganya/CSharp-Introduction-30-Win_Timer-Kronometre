0e2f5ed6f1db92e90a328a47ef96d6a128b6c070
Hakan Akgül <hakanakgul34@gmail.com>
2017-09-28 20:45:19 +0300

    created at 14.08.2017 (dd.mm.yyyy)

---
+++ 20170814-2Win_TimerZamanlayıcı-Kronometre/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace _20170814_2Win_TimerZamanlayıcı_Kronometre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblSaat = new System.Windows.Forms.Label();
+            this.lblDakika = new System.Windows.Forms.Label();
+            this.lblSaniye = new System.Windows.Forms.Label();
+            this.lblGun = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblSaat
+            // 
+            this.lblSaat.AutoSize = true;
+            this.lblSaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblSaat.Location = new System.Drawing.Point(43, 35);
+            this.lblSaat.Name = "lblSaat";
+            this.lblSaat.Size = new System.Drawing.Size(44, 31);
+            this.lblSaat.TabIndex = 1;
+            this.lblSaat.Text = "00";
+            // 
+            // lblDakika
+            // 
+            this.lblDakika.AutoSize = true;
+            this.lblDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblDakika.Location = new System.Drawing.Point(135, 35);
+            this.lblDakika.Name = "lblDakika";
+            this.lblDakika.Size = new System.Drawing.Size(44, 31);
+            this.lblDakika.TabIndex = 2;
+            this.lblDakika.Text = "00";
+            // 
+            // lblSaniye
+            // 
+            this.lblSaniye.AutoSize = true;
+            this.lblSaniye.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblSaniye.Location = new System.Drawing.Point(227, 35);
+            this.lblSaniye.Name = "lblSaniye";
+            this.lblSaniye.Size = new System.Drawing.Size(44, 31);
+            this.lblSaniye.TabIndex = 3;
+            this.lblSaniye.Text = "00";
+            // 
+            // lblGun
+            // 
+            this.lblGun.AutoSize = true;
+            this.lblGun.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblGun.Location = new System.Drawing.Point(135, 147);
+            this.lblGun.Name = "lblGun";
+            this.lblGun.Size = new System.Drawing.Size(44, 31);
+            this.lblGun.TabIndex = 4;
+            this.lblGun.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 262);
+            this.Controls.Add(this.lblGun);
+            this.Controls.Add(this.lblSaniye);
+            this.Controls.Add(this.lblDakika);
+            this.Controls.Add(this.lblSaat);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblSaat;
+        private System.Windows.Forms.Label lblDakika;
+        private System.Windows.Forms.Label lblSaniye;
+        private System.Windows.Forms.Label lblGun;
+    }
+}
+
---
+++ 20170814-2Win_TimerZamanlayıcı-Kronometre/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _20170814_2Win_TimerZamanlayıcı_Kronometre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        byte saniye = 0, dakika = 0 , saat = 0 , gun = 0;
+         
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            lblSaniye.Text = saniye.ToString();
+            lblDakika.Text = dakika.ToString();
+            lblSaat.Text = saat.ToString();
+            lblGun.Text = gun.ToString();
+
+            saniye++;
+            if (saniye >= 60)
+            {
+                saniye = 0;
+                dakika++;
+                if (dakika >= 60)
+                {
+                    dakika = 0;
+                    saat++;
+                    if (saat >= 24)
+                    {
+                        saat = 0;
+                        gun++;
+                    }
+                }
+            }
+        }
+    }
+}
